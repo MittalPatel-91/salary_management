6fdae160d8063ad860335d1952adae159298730d
Mittal Patel <mittal8991@gmail.com>
2026-02-26 18:27:10 +0530

    added employee table with required constraints and indexes

---
+++ db/migrate/20260226123729_create_employees.rb
@@ -0,0 +1,14 @@
+class CreateEmployees < ActiveRecord::Migration[8.1]
+  def change
+    create_table :employees do |t|
+      t.string :full_name, null: false
+      t.string :job_title, null: false
+      t.string :country, null: false
+      t.decimal :salary, null: false, precision: 15, scale: 2
+
+      t.timestamps
+    end
+    add_index :employees, :country
+    add_index :employees, :job_title
+  end
+end
